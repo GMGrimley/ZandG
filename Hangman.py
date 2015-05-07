e4aa6f13755a460ed9b24851774dc419d7e8d332
zackgrimley <grimzack@gmail.com>
2015-05-06 21:14:15 -0500

    Z: This is a test of the emergency broadcast system.

---
+++ Hangman.py
@@ -0,0 +1,10 @@
+# Sample Hangman class for practicing with GitHub
+# Author: Zack
+
+class Hangman:
+	"""This is not a real Hangman class"""
+	man = "hang"
+	correct_answer = "Amalgamation"
+	def f(self):
+		# This is the initialization method for a Hangman game
+		return "Nice Job!"
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This is a test of GitHub.
+
+-Z
