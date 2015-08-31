39dd6068d6c1676a030f2976706e8777cd9c94ba
grimzack <grimzack@gmail.com>
2015-08-30 19:22:48 -0500

    Added a brief list of words, and made Hangman choose one at random

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,22 @@
+import random
+
 # Sample Hangman class for practicing with GitHub
 # Author: Zack
-
-class Hangman:
+class Hangman(object):
 	"""This is not a real Hangman class"""
-	man = "hang"
-	correct_answer = "Amalgamation"
-	def f(self):
-		# This is the initialization method for a Hangman game
-		return "Nice Job!"
+	
+	# This is the initialization method for a Hangman game
+	def __init__(self):
+		self.health = 0
+
+wordlist = []
+
+# Open the list of words to read from
+fd = open('hangmanDictionary.txt', 'r')
+for line in fd:
+	# Save the words to a list
+	wordlist.append(line)
+
+# Get a random word to guess
+word = wordlist[random.randint(0, len(wordlist))]
+
