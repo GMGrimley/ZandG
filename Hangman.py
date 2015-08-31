f373940034c9f1cfb28bc8237564efe5db3f6da6
grimzack <grimzack@gmail.com>
2015-08-30 20:25:42 -0500

    Re-arranged word import logic. Added very simple user input.

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,11 @@
 import random
 
-# Sample Hangman class for practicing with GitHub
+# debug == true enables strange print statements for data validation
+debug = True
+
+# Sample Hangman class for practicing programming and python
+# TODO: Add board drawing class
+#		Add more words
 # Author: Zack
 class Hangman(object):
 	"""This is not a real Hangman class"""
@@ -9,14 +14,62 @@ class Hangman(object):
 	def __init__(self):
 		self.health = 0
 
-wordlist = []
+"""
+	The method which carries out the turn-based play of Hangman game
+"""
+def play():
 
-# Open the list of words to read from
-fd = open('hangmanDictionary.txt', 'r')
-for line in fd:
-	# Save the words to a list
-	wordlist.append(line)
+	wordlist = []
 
-# Get a random word to guess
-word = wordlist[random.randint(0, len(wordlist))]
+	# Open the list of words to read from
+	fd = open('hangmanDictionary.txt', 'r')
+	for line in fd:
+		# Save the words to a list
+		wordlist.append(line)
 
+	# Get a random word to guess
+	word = wordlist[random.randint(0, (len(wordlist) - 1))]
+
+	###############
+	if debug:
+		print word
+	###############
+
+	# Have we won the game? 
+	won = False
+
+	# We've only won if we've guessed all the letters
+	all_guesses = False
+
+	# Keep track of all the letters that have been guessed
+	guess = []
+
+	while (not won):
+		guess.append(raw_input('Please guess a letter: '))
+		for letter in word:
+
+			##############################################################
+			if debug:
+				print('Checking... is {} == {}'.format(guess[-1], letter))
+			##############################################################
+
+			if guess[-1] == letter:
+				# Update board with a correct guess
+				if all_guesses:
+					won = True
+
+				###################
+				if debug:
+					print "Correct!"
+				###################
+
+			else:
+				# Update board with an incorrect guess
+
+				
+				######################
+				if debug:
+					print "Incorrect!"
+				######################
+
+play()
